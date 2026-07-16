eaa09dca2cfa67586c7dea9a059808a6185a7ee7
StarCatcher23 <dreamandachievetoday@gmail.com>
2026-07-15 22:40:47 -0400

    add search form in blog page

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,17 +1,33 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./Blog.css";
 import aboutBg from "../../assets/Rectangle 4.png";
 
 const Blog = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <div className="blog-page">
+      {/* HEADER */}
       <section className="blog-header">
-        <section className="blog-background-image"
-                    style={{ backgroundImage: `url(${aboutBg})` }}
-                    >
+        <section
+          className="blog-background-image"
+          style={{ backgroundImage: `url(${aboutBg})` }}
+        >
           <h1>Articles</h1>
         </section>
       </section>
+
+      {/* SEARCH FORM */}
+      <div className="blog-search">
+        <input
+          type="text"
+          placeholder="Search articles..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+
+      {/* Later: filter your posts using searchTerm */}
     </div>
   );
 };
