b64b55d3942428e4d8d6466e150a64b98300f174
StarCatcher23 <dreamandachievetoday@gmail.com>
2026-08-02 19:27:40 -0400

    remove unused components

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NavBar from "./components/NavBar";
-import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import Blog from "./pages/Blog";
-import Portfolio from "./pages/Portfolio";
 
 function App() {
-  return (
-    <Router>
-      <NavBar />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/blog" element={<Blog />} />
-      </Routes>
-
-      <Footer />
-    </Router>
-  );
+  return <></>;
 }
 
 export default App;
