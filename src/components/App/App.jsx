eb03d5034abffde575b345c093049e8698d820f5
StarCatcher23 <dreamandachievetoday@gmail.com>
2026-08-02 21:17:38 -0400

    remove duplicate BrowerRouter

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import NavBar from "../Navigation/NavBar";
 
 import Home from "../../pages/Home/Home";
@@ -9,7 +9,7 @@ import Contact from "../../pages/Contact/Contact";
 
 function App() {
   return (
-    <Router>
+    <>
       <NavBar />
 
       <Routes>
@@ -19,7 +19,7 @@ function App() {
         <Route path="/blog" element={<Blog />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App/App.jsx";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>,
 );
