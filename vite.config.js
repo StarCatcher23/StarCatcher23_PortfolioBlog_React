d174880d44be27b916bc053b58dc97ef8839ad06
StarCatcher23 <dreamandachievetoday@gmail.com>
2026-08-02 20:53:39 -0400

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/StarCatcher23_PortfolioBlog_React/",
   plugins: [react()],
   build: {
     cssMinify: "esbuild",
