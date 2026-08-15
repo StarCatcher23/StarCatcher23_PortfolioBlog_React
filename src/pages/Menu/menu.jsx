5aa62ffff81aa305d65993b115a95e0a1c9de0ba
StarCatcher23 <dreamandachievetoday@gmail.com>
2026-08-15 14:42:02 -0400

    corrected menu.css to allow fropdown hover to extend

--- src/pages/Menu/menu.jsx
+++ src/pages/Menu/menu.jsx
@@ -1,11 +1,35 @@
 import "./menu.css";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
-const Menu = () => {
-  return (
-    <div className="menu">
-      <h1>Menu</h1>
-    </div>
-  );
-};
+export default function Menu() {
+  const [open, setOpen] = useState(false);
 
-export default Menu;
+<li
+  className="dropdown"
+  onMouseEnter={() => setOpen(true)}
+  onMouseLeave={() => setOpen(false)}
+>
+  <button className="dropdown-btn">Menu ▼</button>
+
+  {open && (
+    <ul className="dropdown-menu">
+      <li>
+        <Link to="/home">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About Me</Link>
+      </li>
+      <li>
+        <Link to="/portfolio">Portfolio</Link>
+      </li>
+      <li>
+        <Link to="/blog">Blog</Link>
+      </li>
+      <li>
+        <Link to="/contact">Contact</Link>
+      </li>
+    </ul>
+  )}
+</li>
+}
